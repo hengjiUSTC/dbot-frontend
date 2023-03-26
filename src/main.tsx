b442564d0ea6474a99bba86ab07505ffbc3a7c8f
hengJ <jihengcu@gmail.com>
2023-03-26 18:03:05 +0800

    add inject

--- src/main.tsx
+++ src/main.tsx
@@ -7,14 +7,16 @@ import { BrowserRouter } from 'react-router-dom';
 import ScrollToTop from './components/scroll-to-top';
 import Router from './routes';
 import { HelmetProvider } from 'react-helmet-async';
+import { inject } from '@vercel/analytics';
 // lazy image
 import 'react-lazy-load-image-component/src/effects/blur.css';
+
+inject();
 
 const container = document.getElementById('root');
 if (!container) throw new Error("Could not find root element with id 'root'");
 // export const baseUrl = import.meta.env.BASE_URL;
 export const baseUrl = '/';
-
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
